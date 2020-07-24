f8b02798ec2949f8702b98b7b323e2d67b4cf985
Vitalii Stepchyk <vitalatron@gmail.com>
2020-07-24 16:25:50 +0300

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -3,6 +3,12 @@ alias date-iso8601="date -u +\"%Y-%m-%dT%H:%M:%SZ\""
 alias xclip="xclip -selection c"
 alias shrug="echo '¯\_(ツ)_/¯'"
 
+# GPU
+alias gpu_on="sudo tee /proc/acpi/bbswitch <<< ON"
+alias gpu_off="sudo tee /proc/acpi/bbswitch <<< OFF"
+alias gpu_status="cat /proc/acpi/bbswitch"
+
+# Functions
 function pngpress() {
     find "$1" -iname '*.png'  | xargs -I % zopflipng -y --iterations=25 --filters=01234mepb --lossy_8bit --lossy_transparent % %
 }
