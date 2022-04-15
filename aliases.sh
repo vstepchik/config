8d3d6fbf3ff995b40799dbc909619f7103f313f3
Vitalii Stepchyk <vitalatron@gmail.com>
2022-04-15 10:27:17 +0200

    modified aliases a bit

--- aliases.sh
+++ aliases.sh
@@ -1,7 +1,16 @@
+# Env
+CARGO_HOME="$HOME/.cargo"
+PATH="$PATH:$CARGO_HOME/bin"
+
+# Misc
 alias timestamp="date +\"%Y%m%d%H%M\""
 alias date-iso8601="date -u +\"%Y-%m-%dT%H:%M:%SZ\""
-alias xclip="xclip -selection c"
 alias shrug="echo '¯\_(ツ)_/¯'"
+alias qr="qrencode -t ansiutf8"
+
+# Command defaults
+alias xclip="xclip -selection c"
+alias dust="dust -br"
 
 # GPU
 alias gpu_on="sudo tee /proc/acpi/bbswitch <<< ON"
@@ -15,4 +24,8 @@ function pngpress() {
 
 function genpass() {
     cat /dev/urandom | tr -dc '[:alnum:][:punct:]' | fold -w ${1:-20} | head -n 1 | tee /dev/tty | tr -d "\n" | xclip -sel clip
+}
+
+function objectid() {
+    cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 24 | head -n 1 | tee /dev/tty | tr -d "\n" | xclip -sel clip
 }
