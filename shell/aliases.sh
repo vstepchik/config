2e34bd21c1741edf52a04306a55e3aa8dcb980eb
Vitalii Stepchyk <vitalatron@gmail.com>
2026-03-08 22:00:01 +0100

    rm gpu-related aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -7,8 +7,3 @@ alias qr="qrencode -t ansiutf8"
 # Command defaults
 alias xclip="xclip -selection c"
 alias dust="dust -br"
-
-# GPU
-alias gpu_on="sudo tee /proc/acpi/bbswitch <<< ON"
-alias gpu_off="sudo tee /proc/acpi/bbswitch <<< OFF"
-alias gpu_status="cat /proc/acpi/bbswitch"
